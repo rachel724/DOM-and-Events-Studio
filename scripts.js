88bbee679a2030126e3349aab1a1dfdf3eec1ffb
Rachel <raitkens724@gmail.com>
2019-11-25 19:14:31 -0600

    step 1

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+let buttonUp = null;
+let buttonDown = null;
+let buttonRight = null;
+let buttonLeft = null;
+let buttonTakeOff = null;
+
+function init () {
+    buttonTakeOff = document.getElementById("takeoff");
+    buttonTakeoff.addEventListener("click", function (event) {
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
+      });
+}
+
+window.onload = init;
