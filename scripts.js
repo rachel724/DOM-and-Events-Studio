30799f4f4ffbb766484dc34cfb793276b96b37ae
Rachel <raitkens724@gmail.com>
2019-11-25 20:36:17 -0600

    finished

--- scripts.js
+++ scripts.js
@@ -14,18 +14,24 @@ let buttonAbort = null;
 function init () {
     buttonTakeOff = document.getElementById("takeoff");
     status = document.getElementById("flightStatus");
-    background = document.querySelector("#shuttleBackground");
+    background = document.getElementById("shuttleBackground");
     height = document.getElementById("spaceShuttleHeight");
     buttonLand = document.getElementById("landing");
     buttonAbort = document.getElementById("missionAbort");
-    buttonUp = document.getElementsByName("Up");
+    buttonUp = document.getElementById("up");
+    buttonDown = document.getElementById("down");
+    buttonLeft = document.getElementById("left");
+    buttonRight = document.getElementById("right");
+    let currentHeight = Number(height.innerHTML);
+    console.log(typeof(currentHeight));
 
     buttonTakeOff.addEventListener("click", function (event) {
         let confirm = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if(confirm){
             status.innerHTML = "Shuttle in flight.";
             background.style.background = "blue";
-            height.innerHTML = "10,000";
+            height.innerHTML = 10000;
+            currentHeight = Number(height.innerHTML);
         };
       });
       
@@ -33,7 +39,8 @@ function init () {
         window.alert("The shuttle is landing. Landing gear engaged.");
         status.innerHTML = "The shuttle has landed.";
         background.style.background = "green";
-        height.innerHTML = "0";
+        height.innerHTML = 0;
+        currentHeight = Number(height.innerHTML);
       });
 
     buttonAbort.addEventListener("click", function (event) {
@@ -41,13 +48,34 @@ function init () {
         if(confirm){
             status.innerHTML = "Mission aborted.";
             background.style.background = "green";
-            height.innerHTML = "0";
+            height.innerHTML = 0;
+            currentHeight = Number(height.innerHTML);
         };
       });
 
       buttonUp.addEventListener("click", function (event) {
-          
+          background.style.bottom = "10px";
+          background.style.top = "";
+          height.innerHTML = currentHeight + 10000;
+          currentHeight = Number(height.innerHTML);
       });
+
+      buttonDown.addEventListener("click", function (event) {
+        background.style.top = "10px";
+        background.style.bottom = "";
+        height.innerHTML = currentHeight - 10000;
+        currentHeight = Number(height.innerHTML);
+    });
+
+    buttonLeft.addEventListener("click", function (event) {
+        background.style.right = "10px";
+        background.style.left = "";
+    });
+
+    buttonRight.addEventListener("click", function (event) {
+        background.style.left = "10px";
+        background.style.right = "";
+    });
 }
 
 window.onload = init;
